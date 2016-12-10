66cd8e7e8e76da8c5099db7e0afb652856fcdc1d
PuZZleDucK <puzzleduck@gmail.com>
2016-12-10 21:04:01 +1100

    add my default profile

---
+++ profiles/g810-default.sh
@@ -0,0 +1,71 @@
+g810-led -a 00FFFF
+
+g810-led -k logo FF00FF
+g810-led -k esc FF00FF
+g810-led -k f1 FF00FF
+g810-led -k f2 FF00FF
+g810-led -k f3 FF00FF
+g810-led -k f4 FF00FF
+g810-led -k f5 FF00FF
+g810-led -k f6 FF00FF
+g810-led -k f7 FF00FF
+g810-led -k f8 FF00FF
+g810-led -k f9 FF00FF
+g810-led -k f10 FF00FF
+g810-led -k f11 FF00FF
+g810-led -k f12 FF00FF
+
+g810-led -k print FFFF00
+g810-led -k scrolllock FFFF00
+g810-led -k pause FFFF00
+
+g810-led -k insert 00FF00
+g810-led -k home 00FF00
+g810-led -k pageup 00FF00
+g810-led -k pagedown 00FF00
+g810-led -k end 00FF00
+g810-led -k delete 00FF00
+
+g810-led -k tab FF00FF
+g810-led -k capslock FF00FF
+g810-led -k tab FF00FF
+g810-led -k capslock FF00FF
+g810-led -k shiftleft FF00FF
+g810-led -k ctrlleft FF00FF
+g810-led -k winleft FF00FF
+g810-led -k altleft FF00FF
+
+g810-led -k space FF00FF
+
+g810-led -k altright FF00FF
+g810-led -k winright FF00FF
+g810-led -k menu FF00FF
+g810-led -k ctrlright FF00FF
+g810-led -k shiftright FF00FF
+g810-led -k enter FF00FF
+g810-led -k backspace FF00FF
+
+g810-led -k w FF0000
+g810-led -k a FF0000
+g810-led -k s FF0000
+g810-led -k d FF0000
+
+g810-led -k num5 FF0000
+g810-led -k num8 FF0000
+g810-led -k num4 FF0000
+g810-led -k num6 FF0000
+g810-led -k num2 FF0000
+g810-led -k num7 FFFF00
+g810-led -k num9 FFFF00
+g810-led -k num1 FFFF00
+g810-led -k num3 FFFF00
+g810-led -k num0 FFFF00
+g810-led -k num. FFFF00
+g810-led -k numenter FFFF00
+g810-led -k num+ FFFF00
+g810-led -k num- FFFF00
+g810-led -k num/ FFFF00
+g810-led -k num* FFFF00
+g810-led -k numlock FFFF00
+
+
